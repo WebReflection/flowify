ce41b0829fa4b470d1e86387ce3f05707caa1692
Andrea Giammarchi <andrea.giammarchi@mailonline.co.uk>
2016-08-03 20:02:15 +0100

    dropped the mustard

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 'use strict';
 
-var babelCore = require('babel-core');
-var babelConfig = {plugins: ['transform-flow-strip-types']};
-var through = require('through');
+var
+  babelCore = require('babel-core'),
+  // configure it as clean and fast as possible
+  babelConfig = {
+    ast: false,
+    babelrc: false,
+    compact: false,
+    comments: false,
+    plugins: ['transform-flow-strip-types']
+  },
+  through = require('through')
+;
 
 module.exports = function(file) {
-    var data = '';
-    return through(
-        function write(buf) {
-            data += buf;
-        },
-        function end() {
-            try {
-                data = babelCore.transform(data, babelConfig).code;
-            } catch(e) {
-                this.emit('error', e.message);
-            }
-            this.queue(data);
-            this.queue(null);
-        }
-    );
+  var data = '';
+  return through(
+    function write(buf) {
+      data += buf;
+    },
+    function end() {
+      try {
+        data = babelCore.transform(data, babelConfig).code;
+      } catch(e) {
+        this.emit('error', '[flow-strip-types] ' + file + ': ' + e);
+      }
+      this.queue(data);
+      this.queue(null);
+    }
+  );
 };
